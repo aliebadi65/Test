eea781817b42f78ef69269cc4fd22d99ab1c0aa3
TestMaster12 <aryapc320@yahoo.com>
2016-05-06 14:05:24 +0000

    Added files via upload

---
+++ plugins/sudoers.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return [[
+#Sudoers!
+  
+  1.@The_DeSigN3r🌐 #Developer ( use #adddeveloper command to add #The_DeSigN3r in your Groups & SuperGroups. )
+  
+  2.@ParsaAlemi🌐 #Manager ( use #addmanager command to add #ParsaAlemi in your Groups & SuperGroups. )
+  
+]]
+end
+return {
+  description = " ", 
+  usage = " ",
+  patterns = {
+    "^[#!/]sudoers$",
+
+  },
+  run = run
+}
+end
